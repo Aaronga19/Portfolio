59c2082052353a91e50467f5142f903ea2364dfd
Aarón Juárez <66045880+Aaronga19@users.noreply.github.com>
2021-07-26 20:37:32 -0500

    Portfolio finished. it's ready to deploy it!

--- Portfolio/applications/users/forms.py
+++ Portfolio/applications/users/forms.py
@@ -11,3 +11,40 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = UserContact
         fields= ('__all__')
+        widgets = {
+            'name': forms.TextInput(
+                attrs={
+                'placeholder':'Full Name',
+                'class':'form-control',
+                'type':'text',
+                }
+            ),
+            'email': forms.EmailInput(
+                attrs={
+                'placeholder':'E-Mail',
+                'class':'form-control', 
+                }
+            ),
+            'phone': forms.TextInput(
+                attrs={
+                'placeholder':'Phone Number',
+                'class':'form-control',
+                'type':'text',
+                }
+            ),
+            'company': forms.TextInput(
+                attrs={
+                'placeholder':'Company or Personal?',
+                'class':'form-control',
+                'type':'text',
+                }
+            ),
+            'message': forms.Textarea(
+                attrs={
+                'placeholder':'Let me know how can I help you ☺',
+                'class':'form-control',
+                'aria-label':"With textarea",
+                }
+            ),
+            
+        }
