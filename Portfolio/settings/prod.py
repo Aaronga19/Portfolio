0680d0928eb7fa6f49dfa43edbf5825d2c864024
Aarón Juárez <66045880+Aaronga19@users.noreply.github.com>
2021-12-21 15:20:02 -0600

    pipeline build

--- Portfolio/settings/prod.py
+++ Portfolio/settings/prod.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['aaronportfol.herokuapp.com']
+ALLOWED_HOSTS = ['aaronportfol.herokuapp.com', "127.0.0.1"]
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
